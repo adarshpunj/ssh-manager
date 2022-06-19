6890f52bfa0127ba893cfb17cf36cc3d496a4409
Adarsh Punj <adarshpunj@Adarshs-MacBook-Pro.local>
2022-06-19 06:36:47 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,88 @@
+'''
+Simple SSH manager to log into your 
+remote servers in one click
+
+Author: Adarsh Punj
+June 2022
+'''
+
+from dataclasses import dataclass
+import subprocess
+
+import preferences as prf
+
+import rumps
+
+@dataclass
+class ServerModel:
+
+	server_name: str
+	username: str
+	ip: str
+	key_location: str
+
+	@property
+	def ssh_command(self) -> str:
+		return f"sudo ssh -i '{self.key_location}' {self.username}@{self.ip}"
+
+class SshManagerApp(rumps.App):
+
+    """
+    rumps.App
+    """
+
+    def __init__(self):
+        super(SshManagerApp, self).__init__(name="SSH Manager")
+
+        self.title = "ssʜ"
+
+        self.menu = [
+            *self.menu_list,
+            None,
+            rumps.MenuItem('Manage servers...',callback=self.edit_preferences)
+            ]
+
+    @property
+    def menu_list(self) -> list:
+
+        '''
+        Returns a list of rumps.MenuItem objects
+        from a list of dictionaries
+        '''
+        _server_list = self._server_list
+
+        return [
+
+            rumps.MenuItem(
+                ServerModel(**x).server_name
+                , callback=self.exec_ssh_command_by_server_name
+
+                ) for x in _server_list
+            ]
+
+    @classmethod
+    def exec_ssh_command_by_server_name(self, sender, sound=prf.PLAY_SOUND) -> None:
+
+        for item in prf.AVAILABLE_SERVERS:
+            server = ServerModel(**item)
+            subprocess.run(
+                ['osascript','-e',f'tell app "Terminal" to do script ("{server.ssh_command}")']
+            ) if server.server_name == sender.title.strip() else None
+
+            if sound:
+                subprocess.run(['afplay',prf.DEFAULT_SOUND])
+
+    @classmethod
+    def edit_preferences(self, sender) -> None:
+
+        subprocess.run(
+            ['open','./preferences.py']
+        )
+
+
+    @property
+    def _server_list(self) -> [dict]:
+        return prf.AVAILABLE_SERVERS
+
+if __name__ == '__main__':
+	SshManagerApp().run()
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+APP = ['app.py']
+DATA_FILES = ['preferences.py']
+OPTIONS = {
+    'argv_emulation': True,
+    'iconfile': 'ssh-manager.icns',
+    'plist': {
+        'CFBundleShortVersionString': '1.0.0',
+        'LSUIElement': True,
+    },
+    'packages': ['rumps']
+}
+
+setup(
+    app=APP,
+    name='SSH Manager',
+    py_modules=[''],
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'], install_requires=['rumps']
+)
